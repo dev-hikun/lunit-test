3a283d38bba490016e6a270abbd239dc87cb4e6c
이희현 <defunc@bliex.com>
2021-05-29 01:18:34 +0900

    #6 다각형 List로 보이도록 설정 1. list component화 2. list over 및 out 이벤트 추가 3. list css 적용

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,11 +1,10 @@
 import Switch from 'components/Switch';
-import useShape from 'hooks/useShape';
 import { Mode } from 'interface';
 import React, { useCallback, useState } from 'react';
 import CanvasDrawingArea from './CanvasDrawingArea';
+import List from './List';
 
 const Main = () => {
-  const { shapes } = useShape();
   const [mode, setMode] = useState<Mode>('drawing');
 
   const onChangeMode = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,11 +21,7 @@ const Main = () => {
         <CanvasDrawingArea mode={mode} />
       </div>
       <div className="list-wrap">
-        <ul>
-          {shapes.map((shape, index) => {
-            return <li key={shape.id}>{`polygon-${index + 1}`}</li>;
-          })}
-        </ul>
+        <List />
       </div>
     </div>
   );
